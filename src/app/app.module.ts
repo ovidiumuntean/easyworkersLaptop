7ee61769ce3670b350aaa8ba1b2ea402096d86ed
ovidiumuntean <ovidiumuntean2006@gmail.com>
2018-04-12 09:57:41 +0100

    sidebar, apply for job

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,33 @@ import { FormsModule } from '@angular/forms';
 // import {DatePickerModule} from 'ng2-datepicker-bootstrap';
 import { HttpModule } from '@angular/http';
 import {RouterModule, Routes} from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatCardModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { RegisterComponent } from './components/register/register.component';
+import { RegisterComponent } from './pages/user/register/register.component';
 import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ProfileComponent } from './components/profile/profile.component';
+import { LoginComponent } from './pages/user/login/login.component';
+import { DashboardComponent } from './pages/user/dashboard/dashboard.component';
+import { ProfileComponent } from './pages/user/profile/profile.component';
 
 import { ValidateService } from './services/validate.service';
 import { AuthService } from './services/auth.service';
 import { FlashMessagesModule } from 'angular2-flash-messages';
 import { AuthGuard } from './guards/auth.guard';
 
-import { EmployeeRegisterComponent } from './components/employee-register/employee-register.component';
-import { CompanyRegisterComponent } from './components/company-register/company-register.component';
-import { CompanyHomeComponent } from './components/company-home/company-home.component';
-import { CompanyProfileComponent } from './components/company-profile/company-profile.component';
-import { CompanyLoginComponent } from './components/company-login/company-login.component';
-import { CreateJobComponent } from './components/create-job/create-job.component';
-import { ViewCompanyJobsComponent } from './components/view-company-jobs/view-company-jobs.component';
+import { EmployeeRegisterComponent } from './pages/company/employee-register/employee-register.component';
+import { CompanyRegisterComponent } from './pages/company/company-register/company-register.component';
+import { CompanyHomeComponent } from './pages/company/company-home/company-home.component';
+import { CompanyProfileComponent } from './pages/company/company-profile/company-profile.component';
+import { CompanyLoginComponent } from './pages/company/company-login/company-login.component';
+import { CreateJobComponent } from './pages/company/create-job/create-job.component';
+import { ViewJobsComponent } from './pages/job/view-jobs/view-jobs.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ViewJobComponent } from './pages/job/view-job/view-job.component';
+import { JobApplicationsComponent } from './pages/job/job-applications/job-applications.component';
 
 const appRoutes: Routes =  [
   {path:'', component: HomeComponent},
@@ -35,10 +41,11 @@ const appRoutes: Routes =  [
   {path:'company/login', component: CompanyLoginComponent},
   {path:'company/profile', component: CompanyProfileComponent, canActivate:[AuthGuard]},
   {path:'company/createJob', component: CreateJobComponent, canActivate:[AuthGuard]},
-  {path:'company/jobs', component: ViewCompanyJobsComponent, canActivate:[AuthGuard]},
+  {path:'company/jobs', component: ViewJobsComponent, canActivate:[AuthGuard]},
   {path:'company', component: CompanyHomeComponent},
   {path:'dashboard', component: DashboardComponent, canActivate:[AuthGuard]},
-  {path:'profile', component: ProfileComponent, canActivate:[AuthGuard]}
+  {path:'profile', component: ProfileComponent, canActivate:[AuthGuard]},
+  {path:'job/view-job', component: ViewJobComponent, canActivate:[AuthGuard]}
 ]
 
 @NgModule({
@@ -56,7 +63,10 @@ const appRoutes: Routes =  [
     CompanyProfileComponent,
     CompanyLoginComponent,
     CreateJobComponent,
-    ViewCompanyJobsComponent
+    ViewJobsComponent,
+    SidebarComponent,
+    ViewJobComponent,
+    JobApplicationsComponent
   ],
   imports: [
     BrowserModule,
@@ -64,7 +74,12 @@ const appRoutes: Routes =  [
     // DatePickerModule,
     HttpModule,
     RouterModule.forRoot(appRoutes),
-    FlashMessagesModule
+    FlashMessagesModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [ValidateService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
